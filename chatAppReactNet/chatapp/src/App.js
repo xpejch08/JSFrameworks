d4010d1745a4b66556fe4b19ef1b53080567b4ae
xpejch08 <pejcharstepan@gmail.com>
2024-01-10 23:51:25 +0100

    rename

---
+++ chatAppReactNet/chatapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ChatApp from './chatapp'; // Import the ChatApp component
+
+function App() {
+  return (
+    <div className="App">
+      <ChatApp /> {/* Render the ChatApp component */}
+    </div>
+  );
+}
+
+export default App;
